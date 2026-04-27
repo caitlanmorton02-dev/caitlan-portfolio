d63cec3e6ff9e02f3a87837381d5b0bf7e379675
caitlanmorton02-dev <caitlanmorton02@gmail.com>
2026-04-27 21:11:36 +0100

    adding testimonials

---
+++ content/testimonials.ts
@@ -0,0 +1,18 @@
+export const testimonials = [
+  {
+    quote: "Thoughtful, logical, and built with technical feasibility in mind - making collaboration with QA significantly smoother.   ",
+    author: "Quality Assurance Manager",
+  },
+  {
+    quote: "Sharp, customer-first thinking - balancing stakeholder vision with thoughtful, user-centred design.",
+    author: "Product Owner",
+  },
+  {
+    quote: "Balances business goals, user needs, and delivery realities - while consistently pushing for high-quality outcomes.",
+    author: "UX Designer (Direct Report)",
+  },
+  {
+    quote: "Built strong partnerships between UX and engineering -  making collaboration feel natural, efficient, and scalable.",
+    author: "Senior Frontend Engineer",
+  },
+]
